7992646555704dfed08327dab3e61bdb60584edf
Bryan-Elliott Tam <bryanelliott.tam@ugent.be>
2025-07-03 13:27:34 +0200

    remove compilation files

--- src/ConstructOracleInput.d.ts
+++
@@ -1,6 +0,0 @@
-import { ITopologyOutput } from "./DataIngestor";
-export declare class ConstructOracleInput {
-    static run(topology: ITopologyOutput, queryToRcc: Record<string, number>): Record<string, number>;
-    private static getShortestPaths;
-    private static convertToGraph;
-}
